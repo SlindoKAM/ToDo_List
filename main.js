1c8cc4f52962c20a6f97e02a017032801a3409f0
Slindo <sngcobo@dvtsoftware.com>
2025-01-08 11:54:21 +0200

    Starting of my project of to-do-list of a side branch to merge if later

---
+++ main.js
@@ -0,0 +1,3 @@
+// console.log('Slindo_55') for logging messeges
+// console.warn('This is a warning') for warning messages in orange. 
+// console.error('When you a=have errors') for error messages in red.
